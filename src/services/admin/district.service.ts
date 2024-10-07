9cca13c05f9840616a8661a0f188e6c7b0e78dc7
nitinchandekar <chand.nitin333@gmail.com>
2024-10-07 20:47:54 +0530

    project code update

--- src/services/admin/district.service.ts
+++ src/services/admin/district.service.ts
@@ -1,136 +1,136 @@
 
-
-import { executeQuery } from "../../config/db/db";
+import { PAGINATION } from "../../constants/constant";
 import { logger } from "../../logger/Logger";
+import {
+    addDistrict,
+    deleteDistrict,
+    getDistrict,
+    getDistrictList,
+    updateDistrict,
+} from "../../services/admin/district.service";
+import { _200, _201, _400, _404 } from "../../utils/ApiResponse";
 
 
 
-/**
- * function getDistrict use for fetch district details
- * @param params 
- * @returns 
- */
-export const addDistrict = async (params: object) => {
-    try {
-        let sql = `INSERT INTO district (DISTRICT_NAME) VALUES (?)`;
-        return executeQuery(sql, params).then(result => {
-            return (result) ? result : null;
-        }).catch(error => {
-            console.error("addDistrict Fetch Data Error: ", error);
-            return null;
+
+export class district {
+
+
+    static async addDistrict(req, res, next) {
+        let districtName = req?.body?.district_name;
+        let response = {};
+        let currentTimestamp = Math.floor(new Date().getTime());
+
+
+        addDistrict([districtName, currentTimestamp]).then((result) => {
+            if (result) {
+
+                _201(res, 'District Added Successfully')
+            } else {
+
+                _400(res, 'District Not Added')
+            }
+        }
+        ).catch((error) => {
+
+            logger.error("addDistrict :: ", error)
+            _400(res, 'District Not Added')
         });
-    } catch (error) {
-        logger.error("addDistrict :: ", error)
-        throw new Error(error)
     }
 
-}
+    static async getDistrict(req, res, next) {
 
-/**
- * @function getDistrict use for fetch district details
- * @param params 
- * @returns 
- */
+        let districtId = req?.params?.id;
+        let response = {};
+        getDistrict([districtId]).then((result) => {
+            if (result) {
 
-export const getDistrict = async (params: object) => {
-    try {
-        let sql = `SELECT DISTRICT_ID,DISTRICT_NAME FROM district WHERE DISTRICT_ID = ?`
-        return executeQuery(sql, params).then(result => {
-            return (result) ? result[0] : null;
-        }).catch(error => {
-            console.error("getDistrict Fetch Data Error: ", error);
-            return null;
+                response['data'] = result;
+                _200(res, 'District Found Successfully', response)
+            } else {
+                _404(res, 'District Not Found')
+            }
+        }).catch((error) => {
+
+            logger.error("getDistrict :: ", error)
+            _404(res, 'District Not Found')
+
         });
-    } catch (error) {
-        logger.error("getDistrict :: ", error)
-        throw new Error(error)
-    }
-}
-
-
-/**
- * @function getDistrictList use for fetch district list
- * @param params 
- * @returns 
- */
-
-export const getDistrictList = async (params: object) => {
-    try {
-        const { limit, offset } = params as { limit: number, offset: number };
-        let sql = `SELECT DISTRICT_ID, DISTRICT_NAME FROM district LIMIT ? OFFSET ?`;
-        return executeQuery(sql, [limit, offset]).then(result => {
-            return (result) ? result : null;
-        }).catch(error => {
-            console.error("getDistrictList Fetch Data Error: ", error);
-            return null;
-        });
-    } catch (error) {
-        logger.error("getDistrictList :: ", error);
-        throw new Error(error);
     }
 
-}
 
-/**
- * @function updateDistrict use for update district details
- * @param params 
- * @returns 
- */
+    static async getDistrictList(req, res, next) {
+        let response = {};
+        let page = parseInt(req.body.page_number) || 1;
+        let limit = PAGINATION.LIMIT || 10;
+        let offset = (page - 1) * limit;
+        getDistrictList({ limit: limit, offset: offset }).then((result) => {
+            if (result) {
 
-export const updateDistrict = async (params: object) => {
-    try {
-        let sql = `UPDATE district SET DISTRICT_NAME = ? WHERE DISTRICT_ID = ?`
-        return executeQuery(sql, params).then(result => {
-            return (result) ? result : null;
-        }).catch(error => {
-            console.error("updateDistrict Fetch Data Error: ", error);
-            return null;
+
+                response['data'] = result;
+                response['page'] = page;
+                response['limit'] = limit;
+                _200(res, 'District List Found Successfully', response)
+            } else {
+                _400(res, 'District List Not Found')
+            }
+        }).catch((error) => {
+
+            logger.error("getDistrictList :: ", error);
+            _400(res, 'District List Not Found')
         });
-    } catch (error) {
-        logger.error("updateDistrict :: ", error)
-        throw new Error(error)
     }
-}
 
-/**
- * @function deleteDistrict use for delete district details
- * @param params 
- * @returns 
- */
+    static async updateDistrict(req, res, next) {
+        let districtId = req?.body?.district_id;
+        let districtName = req?.body?.district_name;
+        let response = {};
+        let currentTimestamp = Math.floor(new Date().getTime());
+        getDistrict([districtId]).then((result) => {
+            if (!result) {
+                return _404(res, 'District Not Found');
+            }
+            updateDistrict([districtName, districtId]).then((result) => {
+                if (result) {
+                    _200(res, 'District Updated Successfully');
+                } else {
+                    _400(res, 'District Not Updated')
+                }
+            }).catch((err) => {
 
-export const deleteDistrict = async (params: object) => {
-    try {
-        // let sql = `DELETE FROM district WHERE DISTRICT_ID = ?`
-        let sql = `UPDATE district SET IS_DELETE = 1 WHERE DISTRICT_ID = ?`
-        return executeQuery(sql, params).then(result => {
-            return (result) ? result : null;
-        }).catch(error => {
-            console.error("deleteDistrict Fetch Data Error: ", error);
-            return null;
+                logger.error("updateDistrict :: ", err)
+                _400(res, 'District Not Updated')
+            });
+        }).catch((error) => {
+            logger.error("updateDistrict :: ", error);
+            _404(res, 'District Not Found');
         });
-    } catch (error) {
-        logger.error("deleteDistrict :: ", error)
-        throw new Error(error)
     }
-}
 
-/**
- * @function getDistrictCount use for fetch district count
- * @param params 
- * @returns 
- */
+    static async deleteDistrict(req: any, res: any, next: any) {
+        let districtId = req?.params?.id;
+        getDistrict([districtId]).then((result) => {
+            if (!result) {
+                return _404(res, 'District Not Found');
+            }
 
-export const getDistrictCount = async (params: object) => {
-    try {
-        let sql = `SELECT COUNT(DISTRICT_ID) AS COUNT FROM district`
-        return executeQuery(sql, params).then(result => {
-            return (result) ? result[0] : null;
-        }).catch(error => {
-            console.error("getDistrictCount Fetch Data Error: ", error);
-            return null;
+            deleteDistrict([districtId]).then((result) => {
+                if (result) {
+                    _200(res, 'District Deleted Successfully');
+                } else {
+                    _400(res, 'District Not Deleted');
+                }
+            }).catch((error) => {
+                logger.error("deleteDistrict :: ", error);
+                _400(res, 'District Not Deleted');
+            });
+        }).catch((error) => {
+            logger.error("deleteDistrict :: ", error);
+            _404(res, 'District Not Found');
         });
-    } catch (error) {
-        logger.error("getDistrictCount :: ", error)
-        throw new Error(error)
     }
+
+
+
 }
