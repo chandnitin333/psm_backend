de84fe3cad117f9a366c3f59718e6f74ac049d53
nitinchandekar <chand.nitin333@gmail.com>
2025-02-07 23:23:43 +0530

    update

--- src/environments/dev.env.ts
+++ src/environments/dev.env.ts
@@ -1,10 +1,10 @@
 import { Environment } from "./env";
 
 export const DevEnvironment: Environment = {
-    DB_HOST: "103.102.234.151",
-    DB_USER: "psm_app",
+    DB_HOST: "localhost",
+    DB_USER: "root",
     DB_PWD: "Java@123",
-    DB_NAME: "psm",
+    DB_NAME: "psm_lts",
     DB_CONNECTION_LIMIT: 10,
     jwt_secret: "8ea0d9d944cce86b979125f999588e850b791e5c5375048cc936da86ac67a4824e844de231f99e0dc61f111cc89d465d"
 };
